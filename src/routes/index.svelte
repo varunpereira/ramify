554d65f942570f6b27b5288264bf6909a7d57bf5
Varun Pereira <68728165+varunpereira@users.noreply.github.com>
2022-06-02 12:38:53 +1000

    1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,40 @@
 <script>
-	var up = 50;
-	var left = 50;
-	var move = 10;
+	import { tweened } from 'svelte/motion';
+	var y = 50;
+	var x = 1;
+	var move = 1;
+	var original = 2 * 60; // TYPE NUMBER OF SECONDS HERE
+	var timer = tweened(original);
+	setInterval(function () {
+		if ($timer > 0) {
+			$timer -= 1;
+		}
+	}, 1000);
+	$: seconds = Math.floor($timer);
 	// key event
 	function typeKey(e) {
-		if (e.keyCode === 37 && left !== 0) {
-			left -= move;
-		} else if (e.keyCode === 38 && up !== 0) {
-			up -= move;
+		if (e.keyCode === 37 && x !== 0) {
+			x -= move;
+		} else if (e.keyCode === 38 && y !== 0) {
+			y -= move;
 		} else if (e.keyCode === 39) {
-			left += move;
+			x += move;
 		} else if (e.keyCode === 40) {
-			up += move;
+			y += move;
 		}
 	}
 </script>
 
 <!-- get scroll x and y check if 0 and 0 -->
+<p class="text-center">Level 1 : {seconds} seconds left!</p>
 <div
 	class="overflow-x-scroll overflow-y-scroll  w-[500px] h-[500px] m-auto relative rounded-lg border-2 border-black"
 >
 	<img
-		style="left:{left}px; top:{up}px;"
+		style="left:{x}px; top:{y}px;"
 		src="/car.jpg"
 		alt="logo"
-		class="w-[50px] h-[50px] absolute rounded-full"
+		class="w-[30px] h-[30px] absolute rounded-full"
 	/>
 </div>
 
